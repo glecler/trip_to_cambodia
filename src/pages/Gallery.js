5b440d24368ee8c7fcd43cc7d629052141806936
Gregoire Lecler <gregoirelecler@gmail.com>
2023-06-04 21:36:21 +0700

    added assets and picture gallery

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,8 +1,16 @@
-import { Box, Text, Heading} from 'grommet';
+import { Box, Text, Heading, Image } from 'grommet';
 import Customheader from '../components/Customheader.js';
 
 
 function Gallery () {
+	
+	function importAll(r) {
+		return r.keys().map(r);
+	}
+
+	const images = Array(importAll(require.context('../assets', false, /\.(png|jpe?g|svg)$/)));
+	const len = images[0].length;
+	console.log(len, images);	
 	return(
 		<Box>
 			<Customheader/>
@@ -19,6 +27,13 @@ function Gallery () {
 					Gallerie
 				</Heading>
 				<Text> Petite sélection de photos prises au cours du séjour </Text>
+			</Box>
+			<Box justify="center" alignContent="center" width="80vw" margin="medium" direction="row" alignSelf="center" wrap>
+				{images[0].map((image) => (
+					<Box alignContent="center" justify="center" width="25vw" margin="xsmall">
+						<Image align="center" src={image}/>
+					</Box>
+					))}
 			</Box>
 		</Box>
 	);
